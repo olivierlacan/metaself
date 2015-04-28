3866bfb0d92afabf7ba59b8bca163532359da4e2
Olivier Lacan <hi@olivierlacan.com>
2015-04-27 22:55:57 -0400

    Update Rails to 4.2.1
    Also add responders gem because I'm lazy like that.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.2.0'
 
-gem 'rails', '4.1.4'
+gem 'rails', '4.2.1'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -20,6 +20,7 @@ gem 'pg'
 gem 'simple_form'
 gem 'thin'
 gem 'zurb-foundation', '~> 4.3.2'
+gem 'responders'
 
 group :development do
   gem 'better_errors'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,9 @@ Rails.application.configure do
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
 
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
